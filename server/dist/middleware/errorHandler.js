7b59407fa5fcef72582665b3691814c094bc8eb2
jhenriquem <machadojh11@gmail.com>
2024-06-24 23:26:11 -0300

    deploy 1

--- server/dist/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function errorHandler(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).json({ status: 500, type: 'Error', message: 'An unexpected error occurred', error: err.message });
-}
-exports.default = errorHandler;
